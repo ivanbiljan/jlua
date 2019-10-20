75e518d045d2ac7791712f8d3a779cd0b116adbf
ivanbiljan <ssbiljan@gmail.com>
2019-10-20 21:32:56 +0200

    Add more Lua constants

--- src/com/jlua/luainterop/LuaConstants.java
+++ src/com/jlua/luainterop/LuaConstants.java
@@ -1,6 +1,8 @@
 package com.jlua.luainterop;
 
 public final class LuaConstants {
-    public static final int REGISTRY_INDEX = -100100;
+    public static final int REGISTRY_INDEX = -1001000;
     public static final int LUA_MULTRET = -1;
+    public static final int REGISTRY_MAINTHREAD_INDEX = 1;
+    public static final int REGISTRY_GLOBALENV_INDEX = 2;
 }
---
+++ src/com/jlua/luainterop/LuaThreadStatus.java
@@ -0,0 +1,5 @@
+package com.jlua.luainterop;
+
+public enum LuaThreadStatus {
+    LUA_OK, LUA_YIELD, LUA_ERRRUN, LUA_ERRSYNTAX, LUA_ERRMEM, LUA_ERRERR, @Deprecated LUA_ERRGCMM
+}
